9dca3daae1b2f20dd77c009b9c789bbe58677030
Almanuel <almanueldelossantos23@gmail.com>
2019-08-27 15:46:03 +0800

    sas

---
+++ test.java
@@ -0,0 +1,20 @@
+
+public class test{
+
+	public static void main(String[] args) {
+		System.out.println("Hello mga tanga");
+		String ss = "tanga tanga";
+		System.out.println(ss);
+
+		if (ss == "asd") {
+			System.out.println("uto uto mo");
+			
+		}
+		else{
+			System.out.println("nc3");
+		}
+		
+
+		
+	}
+}
